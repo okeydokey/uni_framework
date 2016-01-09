a385cb480898a2380bb2320f211e7789218c9bfe
okeydokey <yunkim28@gmail.com>
2016-01-09 23:31:27 +0900

    first publish project for study Spring boot

---
+++ src/main/java/com/okeydokey/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.okeydokey;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UniFrameworkApplication.class);
+	}
+
+}
---
+++ src/main/java/com/okeydokey/UniFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.okeydokey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniFrameworkApplication.class, args);
+	}
+}
